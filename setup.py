30296beabb7a921c03530cc900610feb7ebdca96
MJ-Jang <xkxpa@korea.ac.kr>
2020-07-13 11:19:17 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name='condor',
+    version='0.1',
+    description='Korean spacing corrector',
+    author='MJ Jang',
+    install_requires=required,
+    packages=find_packages(exclude=['docs', 'tests', 'tmp', 'data', '__pycache__']),
+    python_requires='>=3',
+    package_data={},
+    zip_safe=False,
+    include_package_data=True
+)
